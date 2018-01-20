ec9b62fee4b2d1f8625243cac9e48cf61429f451
Hwi Ahn <byron1st@icloud.com>
2018-01-21 00:48:02 +0900

    add react dev tool

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { app, BrowserWindow } from "electron"
 import * as path from "path"
 import * as url from "url"
+import * as os from 'os'
+import * as fs from 'fs'
 
 let mainWindow: Electron.BrowserWindow
 
-function createWindow() {
+function createWindow () {
   mainWindow = new BrowserWindow({
     height: 600,
     width: 800,
@@ -16,11 +18,37 @@ function createWindow() {
       slashes: true,
   }))
 
+  const reactDevToolPath = getReactDevToolPath()
+  BrowserWindow.addDevToolsExtension(reactDevToolPath)
   mainWindow.webContents.openDevTools()
 
   mainWindow.on("closed", () => {
     mainWindow = null
   })
+}
+
+function getReactDevToolPath (): string {
+  const appID = 'fmkadmapgofadopljbjfkapdkoienihi'
+  const appPath = getAppPath(appID)
+
+  const appVersion = fs.readdirSync(appPath)
+  .filter(file => fs.statSync(path.join(appPath, file)).isDirectory())
+  .sort((prev, next) => {
+    if (next > prev) return 1
+    else return -1
+  })[0]
+
+  return path.join(appPath, appVersion)
+}
+
+function getAppPath (appID: string): string {
+  if (os.platform() === 'darwin') {
+    return path.join(os.homedir(), 'Library', 'Application Support', 'Google', 'Chrome', 'Default', 'Extensions', appID)
+  } else if (os.platform() === 'win32') {
+    return path.join(os.homedir(), 'AppData', 'Local', 'Google', 'Chrome', 'User Data', 'Default', 'Extensions', appID)
+  } else {
+    return path.join(os.homedir(), '.config', 'google-chrome', 'Default', 'Extensions', appID)
+  }
 }
 
 app.on("ready", createWindow)
