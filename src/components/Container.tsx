7a359864276b03426dd64d44a452cc44e7aa573c
Hwi Ahn <byron1st@icloud.com>
2018-02-08 16:41:35 +0900

    upgrade outdated packages

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,12 +1,44 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import * as visnetwork from '../visnetwork'
+
+const htmlElementID = {
+  visCanvas: 'vis-canvas',
+  infoPanel: 'info-panel'
+}
 
 class Container extends React.Component {
+  componentDidMount () {
+    const htmlVisCanvas = document.getElementById(htmlElementID.visCanvas)
+    const htmlInfoPanel = document.getElementById(htmlElementID.infoPanel)
+
+    visnetwork.init({ visnetwork: htmlVisCanvas, details: htmlInfoPanel })
+  }
+
   render() {
-    return (
-      <h1>Hello, React!</h1>
-    )
+    return [
+      <div id={htmlElementID.infoPanel} style={style.infoPanel} />,
+      <div id={htmlElementID.visCanvas} style={style.visCanvas} />
+    ]
   }
 }
 
-ReactDOM.render(<Container />, document.getElementById("details"))
+const style = {
+  infoPanel: {
+    backgroundColor: 'aqua',
+    width: '300px',
+    zIndex: 100,
+    // position: 'fixed',
+    // overflowY: 'scroll'
+  },
+  visCanvas: {
+    height: '100%',
+    width: '100%',
+    margin: '0 auto',
+    padding: 0,
+    backgroundColor: 'lightgray',
+    float: 'left'
+  }
+}
+
+ReactDOM.render(<Container />, document.getElementById("container"))
