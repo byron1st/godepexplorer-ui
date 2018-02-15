c56df8dec708b9834ea1101b59ace5ccce0ba583
Hwi Ahn <hwi.ahn@bigpicturelabs.io>
2018-02-15 10:46:20 +0900

    remove src-archived because all contents are migrated to src

--- src-archived/renderer.ts
+++
@@ -1,6 +0,0 @@
-import * as visnetwork from './visnetwork'
-
-const htmlVisnetwork = document.getElementById('visnetwork')
-const htmlDetails = document.getElementById('details')
-
-visnetwork.init({ visnetwork: htmlVisnetwork, details: htmlDetails })
