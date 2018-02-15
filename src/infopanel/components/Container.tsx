ca011777d745258d9224a445eb369af80a9a8608
Hwi Ahn <hwi.ahn@bigpicturelabs.io>
2018-02-15 13:19:03 +0900

    create skeleton code for info panel component

--- src/infopanel/components/Container.tsx
+++ src/infopanel/components/Container.tsx
@@ -1,17 +1,42 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { remote } from 'electron'
+import { GraphElements, Node, Edge } from '../../types'
 
-class Container extends React.Component {
+class Container extends React.Component<{}, GraphElements<Node, Edge>> {
+  constructor (props: {}) {
+    super(props)
+
+    // Initialize the state
+    this.state = {
+      nodes: [],
+      edges: []
+    }
+  }
+
+  update (graphElements: GraphElements<Node, Edge>) {
+    this.setState({ nodes: graphElements.nodes, edges: graphElements.edges })
+  }
+
+  componentDidMount () {
+    const window: Electron.BrowserWindow & { initialData?: any } = remote.getCurrentWindow()
+    if (window.initialData) {
+      this.update(window.initialData)
+    }
+  }
+
   render() {
-    return <h1>Hello World!</h1>
+    console.log(this.state)
+    return <table style={style.table}>
+
+    </table>
   }
 }
 
 const style = {
+  table: {
+    margin: '10px'
+  }
 }
-
-const window: Electron.BrowserWindow & { initialData?: any } = remote.getCurrentWindow()
-console.log(window.initialData)
 
 ReactDOM.render(<Container />, document.getElementById("container"))
