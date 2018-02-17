75025b60e63d6f104223c17e5f5ee08ec336a940
Hwi Ahn <hwi.ahn@bigpicturelabs.io>
2018-02-17 18:47:16 +0900

    refactor types

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,34 @@
-import * as common from './types.common'
 import * as vis from 'vis'
-import * as visTypes from './types.vis'
 
-// Vis specific type compositions
-export interface Node extends common.AbstractNode<visTypes.NodeMetaInfo> {}
-export interface Edge extends common.AbstractEdge<visTypes.EdgeMetaInfo> {}
-export interface Graph extends common.AbstractGraph<Node, Edge> {}
+export interface Node extends vis.Node {
+  meta: NodeMetaInfo
+}
+
+export interface Edge extends vis.Edge {
+  meta: EdgeMetaInfo
+}
+
+export interface Graph {
+  nodes: Node[]
+  edges: Edge[]
+}
+
+interface NodeMetaInfo {
+  packagePath: string
+  packageName: string
+  packageDir: string
+  isPkg: boolean
+  isExternal: boolean
+  isStd: boolean
+  funcSet: { [func: string]: boolean }
+}
+
+interface EdgeMetaInfo {
+  type: 0 | 1
+  count: number
+  depAtFunc: { [key: string]: boolean }
+  arrows?: 'to' | 'from' | 'middle'
+}
 
 // Network-level type compositions (used by the controller module)
 export interface Request { pkgName: string }
