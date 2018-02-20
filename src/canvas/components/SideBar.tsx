19d948ab4cc3df121fa987bf45e62e36381876ca
Hwi Ahn <hwi.ahn@bigpicturelabs.io>
2018-02-20 14:08:27 +0900

    make sidebar resizable

--- src/canvas/components/SideBar.tsx
+++ src/canvas/components/SideBar.tsx
@@ -1,10 +1,34 @@
 import * as React from 'react'
+import Resizable from 're-resizable'
 
 export default class MenuBar extends React.Component {
+  state = {
+    width: 300
+  }
+
+  updateWidth (event: MouseEvent, direction: string, ref: HTMLElement, delta: { width: number, height: number }) {
+    this.setState({ width: this.state.width + delta.width })
+  }
+
   render () {
     return (
-      <div className='position-fixed bg-light' style={{height: 'inherit', width:'300px', zIndex: 1010}}>
-      </div>
+      <Resizable
+        className='position-fixed bg-light'
+        style={style.ResizableComp}
+        size={{ height: '100%', width: this.state.width }}
+        onResizeStop={this.updateWidth.bind(this)}
+        minWidth={300}
+        maxWidth={800}
+      >
+
+      </Resizable>
     )
+  }
+}
+
+const style = {
+  ResizableComp: {
+    zIndex: 1010,
+    borderStyle: 'none outset none none'
   }
 }
