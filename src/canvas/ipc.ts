d1c4b46a757fe1c1091fed1435a6c20fcc18bdb9
Hwi Ahn <hwi.ahn@bigpicturelabs.io>
2018-02-22 19:13:10 +0900

    remove unused code

--- src/canvas/ipc.ts
+++ src/canvas/ipc.ts
@@ -1,7 +1,5 @@
 import { ipcRenderer } from 'electron'
 import * as IPCType from '../IPCTypes'
-import { Graph } from '../GlobalTypes'
-import { graphActions } from './actions'
 
 export function sendDepReq (pkgName: string) {
   ipcRenderer.send(IPCType.GetDepOfPkg.Request, pkgName)
