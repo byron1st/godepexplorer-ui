48517692bc72cbb4538236cd2173ec911a455e5d
Hwi Ahn <hwi.ahn@bigpicturelabs.io>
2018-02-23 00:12:27 +0900

    remove old code

--- src-archived/canvas/reducers/reducers.ts
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux'
-
-import uiReducers from './uiReducer'
-
-export default combineReducers({
-  uiState: uiReducers
-})
--- src-archived/constants.ts
+++
@@ -1,6 +0,0 @@
-export const IPC = {
-  GetDepOfPkg: {
-    Request: 'request-expand-pkg-structure',
-    Response: 'receive-expand-pkg-structure'
-  }
-}
