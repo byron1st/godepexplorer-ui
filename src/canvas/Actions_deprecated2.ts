00a77cea4a9d21af02848c2bef64db69749fd125
byron1st <byron1st@icloud.com>
2018-06-08 18:41:09 +0900

    remove unused comments and packages

--- src/canvas/Actions_deprecated2.ts
+++
@@ -1,2 +0,0 @@
-export * from './actions/UIActions'
-export * from './actions/DataActions'
--- src/canvas/actions/UIActions.ts
+++ src/canvas/actions/UIActions.ts
@@ -94,28 +94,3 @@ export type UIAction =
   | ITurnOnLoadingIndicatorAction
   | ITurnOffLoadingIndicatorAction
   | ITogglePkgImportedAction
-
-// export const uiActions = {
-//   updateWidth: createAction('UPDATE_WIDTH', (newWidth: number) => ({
-//     type: 'UPDATE_WIDTH',
-//     payload: newWidth
-//   })),
-//   updateHeight: createAction('UPDATE_HEIGHT', (newHeight: number) => ({
-//     type: 'UPDATE_HEIGHT',
-//     payload: newHeight
-//   })),
-//   changeNodeVisible: createAction('CHANGE_NODE_VISIBLE'),
-//   changeEdgeVisible: createAction('CHANGE_EDGE_VISIBLE'),
-//   turnOnLoadingIndicator: createAction(
-//     'TURNON_LOADING_INDICATOR',
-//     (packagePath: string) => ({
-//       type: 'TURNON_LOADING_INDICATOR',
-//       payload: packagePath
-//     })
-//   ),
-//   turnOffLoadingIndicator: createAction('TURNOFF_LOADING_INDICATOR'),
-//   togglePkgImported: createAction('TOGGLE_PKG_IMPORTED')
-// }
-//
-// const returnsOfActions = Object.values(uiActions).map(getReturnOfExpression)
-// export type UIAction = typeof returnsOfActions[number]
