8e0035447ac9191006344317add92550bb526c41
byron1st <byron1st@icloud.com>
2018-06-13 02:55:06 +0900

    remove unused comment

--- src/canvas/reducers/GraphReducers.ts
+++ src/canvas/reducers/GraphReducers.ts
@@ -13,13 +13,8 @@ const INITIAL_STATE: IGraphState = {
 export default (state = INITIAL_STATE, action: DataAction) => {
   switch (action.type) {
     case DataActionTypeKey.ADD_NEW_GRAPH:
-      // DataSet.init(action.payload)
       DataSet.addGraph(action.payload)
 
-      // const nodeList = extractSideBarTypeDataFromINodeList(
-      //   action.payload.nodes,
-      //   state.ignoreStd
-      // )
       const nodeList = DataSet.getSideBarTypeData(
         state.nodeList,
         state.ignoreStd
